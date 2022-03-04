c7c877d55402869e167336be98119afd211c308d
Yacine Belkacem <yacine3@live.com>
2022-03-04 22:21:21 +0100

    création header.vue et modification NavLink.vue

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+      <img class="header__logo" src="images/icon-left-font-monochrome-white-redi.png" alt="logo de groupomania">
+    </div>
+</template>
+
+
+<script>
+export default {
+	name: "Header",
+}
+</script>
+
+
+
+<style lang="scss" scoped>
+    .header {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  background-color: rgb(253, 45, 1);
+
+   &__logo{
+      width: 250px;
+      height: auto;
+      align-self: center; 
+    }
+}
+</style>
--- frontend/src/components/NavLink.vue
+++ frontend/src/components/NavLink.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="header">
-      <img class="header__logo" src="images/icon-left-font-monochrome-white-redi.png" alt="logo de groupomania">
-      <nav class="header__nav">
+    <div>
+      <nav class="nav">
         <router-link to="/posts">Les publications</router-link>
         <p> {{user.firstName}} {{user.lastName}}</p>
-        <router-link to="/profile"><img :src="user.image_url" alt="photo de profil" class="header__img"></router-link>
+        <router-link to="/profile"><img :src="user.image_url" alt="photo de profil" class="nav__img"></router-link>
         <button @click="logout()" class="button">
                   Déconnexion
         </button>
@@ -38,28 +37,15 @@ export default {
 
 
 <style lang="scss" scoped>
-    .header {
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  background-color: rgb(253, 45, 1);
-
-    &__nav{
+    .nav{
       display: flex;
       align-items: center;
       justify-content: space-between;
-    }
 
-    &__logo{
-      width: 250px;
-      height: auto;
-      align-self: center; 
-    }
-
-    &__img{
-      border-radius: 30px;
-      height: 60px;
-      width: 60px;
-    }
+        &__img{
+          border-radius: 30px;
+          height: 60px;
+          width: 60px;
+        }
 }
 </style>
