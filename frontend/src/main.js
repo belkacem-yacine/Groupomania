cb35058dd4269070590b1c9262a0e67de14a033d
Yacine Belkacem <yacine3@live.com>
2022-03-16 20:04:02 +0100

    ajout de font awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-createApp(App).use(store).use(router).mount('#app')
+/* import specific icons */
+import { fas /*possible de mettre aussi juste l icon voulu c'est meme mieux*/ } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(fas /*si plusieurs icon a prendre au lieu de fas suivre ce mdl ex: faPaperClip, faArrowLeft*/)
+
+createApp(App)
+.component('fa', FontAwesomeIcon)
+.use(store)
+.use(router)
+.mount('#app')
