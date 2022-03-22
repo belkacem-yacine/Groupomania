5b9d50924584f7c0dfd022a624a2ff0a195efe5e
Yacine Belkacem <yacine3@live.com>
2022-03-22 20:41:39 +0100

    modification du limiter

--- backend/middleware/limit.js
+++ backend/middleware/limit.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 3,
-    message: 'Trop de tentatives de connexion. Compte bloqué pour 2 minutes'
+    max: 5,
+    message: 'Trop de tentatives de connexion. Compte bloqué pour 1 minute'
 });
 
 module.exports = { limiter};
